7070c379872e3011ff22faa6e50c49921935a521
jeat14 <jeat425@gmail.com>
2025-06-04 17:33:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=port,
                           url_path=TOKEN,
-                          webhook_url="https://your-app-name.onrender.com/" + TOKEN)
+                          webhook_url="https://tempmail-bot-ctdi.onrender.com/" + TOKEN)
     print("Bot running")
     updater.idle()
 
